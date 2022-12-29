62cb100cebca9e0d821a6d757276a67643d54e85
Blake <80996688+BlakeTnr@users.noreply.github.com>
2022-12-29 18:28:59 -0500

    Added color & changed text

--- Terminal.js
+++ Terminal.js
@@ -52,27 +52,40 @@ function textInMiddleOfLine(text, spanLength) {
 
 const terminal = new CustomTerminal();
 terminal.loadTerminal();
-terminal.terminal.write(encodedName);
-terminal.terminal.writeln(textInMiddleOfLine(" Software Developer, Cyber-Security Enthusiast & High School Senior ", 128))
-terminal.terminal.writeln("Projects:\n\n\n\r");
+terminal.terminal.write("\x1b[1;92m" + encodedName);
+terminal.terminal.writeln("\x1b[1;92m" + textInMiddleOfLine(" Software Developer, Cyber-Security Enthusiast & High School Senior ", 128))
+terminal.terminal.writeln("\x1b[1;92m" + "\nMy GitHub: https://github.com/BlakeTnr")
+terminal.terminal.writeln("\x1b[1;92m" + "\nProjects:\n")
 loadProjects()
+
+function UnderlineWithSpaces(text) {
+    splitText = text.split(" ")
+
+    ANSIIFormatedText = ""
+    splitText.forEach(part => {
+        ANSIIFormatedText = ANSIIFormatedText + "\x1b[1;4m\x1b[1;92m\x1b[1;100m" + part + "\x1b[1;0m\x1b[1;100m" + " "
+    })
+
+    ANSIIFormatedText = ANSIIFormatedText.substring(0, ANSIIFormatedText.length - 1);
+    return ANSIIFormatedText + "\x1b[1;0m"
+}
 
 function loadProjects() {
     projects = []
-    VulnerableWebApp = new Project("Vulnerable Web App", "A web app that simulates a vulnerable web app application for red team vs blue team competitions", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*")
+    VulnerableWebApp = new Project("Vulnerable Web App", "A web app that simulates a vulnerable web app application for red team vs blue team competitions", "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=0.672xw:1.00xh;0.166xw,0&resize=640:*", "https://github.com/BlakeTnr/UB-Lockdown-Vulnerable-Web-App")
     projects.push(VulnerableWebApp)
-    Perception = new Project("A video game I made about perception", "Uses Unity and math to create mind bending puzzles", null)
+    Perception = new Project("Peception", "A Unity game using math to create math and physics to make mind bending puzzles", null, "https://github.com/BlakeTnr/Perception")
     projects.push(Perception)
-    InternetScoreChecker = new Project("Simulate Web Traffic (InternetScoreChecker)", "Used to simulate 'employee' internet traffic for red team vs blue team competitions. Makes it so blue teams can't turn off access to the internet and still win.", null); // BAD NAME
+    InternetScoreChecker = new Project("Web Traffic Simulator (InternetScoreChecker)", "Used to simulate 'employee' internet traffic for red team vs blue team competitions.\n\r\tMakes it so blue teams can't turn off access to the internet and still win.", null, "https://github.com/BlakeTnr/Internet-Scoring-Client"); // BAD NAME
     projects.push(InternetScoreChecker)
-    Blender = new Project("Some of my 3D art work", "3D blender art", null);
-    projects.push(Blender)
-    TimelineGame = new Project("Some of my 3D art work", "3D blender art", null);
+    Blender = new Project("3D Art Portfolio", "3D blender art portfolio", null, "https://github.com/BlakeTnr/Blender");
     projects.push(Blender)
 
     projects.forEach(project => {
-        terminal.terminal.writeln(project.name)
-        terminal.terminal.writeln(project.description + "\n")
+        
+        terminal.terminal.writeln("\x1b[1;92m" + "   " + UnderlineWithSpaces(project.name))
+        terminal.terminal.writeln("\x1b[1;92m" + "\t" + project.description + "\n")
+        terminal.terminal.writeln("\x1b[1;92m\x1b[1;4m" + "\t" + project.link + "\x1b[1;0m\n\n")
 
     })
     
